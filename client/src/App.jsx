1e275da679eda640cc0682c3f1deefcb9ac2b5e6
soundariyabala <balasoundariya.ravikumar@gmail.com>
2024-02-01 19:11:59 +0530

    client code

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRouters from "../src/routers/AppRouters";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <AppRouters />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
